70a30233ea39d15dfc0ba63e95d5a9cc5c3a2186
Oscar David Garcia Medina <ogarcia@asesoftware.com>
2019-05-20 15:26:52 -0500

    added a login

--- Django/platzigram/posts/admin.py
+++ Django/platzigram/posts/admin.py
@@ -1,3 +1,18 @@
-from django.contrib import admin
+"""Posts admin."""
 
-# Register your models here.
+from django.contrib import admin
+from posts.models import Post
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Posts Admin model"""
+
+    list_display = ('pk', 'user', 'photo')
+    list_display_links = ('pk', 'user')
+    list_editable = ('photo',)
+    list_filter = (
+                'created',
+                'modified'
+    )
+    
---
+++ Django/platzigram/users/apps.py
@@ -0,0 +1,10 @@
+"""User app configuration."""
+
+from django.apps import AppConfig
+
+
+class UsersConfig(AppConfig):
+    """Use app config."""
+
+    name = 'users'
+    verbose_name = 'Users'
