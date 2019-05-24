4670c057c8b8dd1c661b9f674c4bc4f2d41c68e8
Oscar David Garcia Medina <ogarcia@asesoftware.com>
2019-05-24 15:44:34 -0500

    Adding views with the django views templates

--- Django/platzigram/posts/views.py
+++ Django/platzigram/posts/views.py
@@ -2,7 +2,10 @@
 
 # Django
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView
 
 # Forms
 from posts.forms import PostForm
@@ -11,13 +14,25 @@ from posts.forms import PostForm
 from posts.models import Post
 
 
-@login_required
-def list_posts(request):
-    """List existing posts."""
-    posts  = Post.objects.all().order_by('-created')
-    return render(request, 'posts/feed.html', {'posts': posts})
+class PostsFeedView(LoginRequiredMixin, ListView):
+    """Return all published posts."""
 
+    template_name = 'posts/feed.html'
+    model = Post
+    ordering = ('-created',)
+    paginate_by = 5
+    context_object_name = 'posts'
+
+
+    ''' Second version
+    @login_required
+    def list_posts(request):
+        """List existing posts."""
+        posts  = Post.objects.all().order_by('-created')
+        return render(request, 'posts/feed.html', {'posts': posts})
     '''
+
+    ''' Firts version
     content = []
     for post in posts:
         content.append("""
@@ -28,6 +43,30 @@ def list_posts(request):
     return HttpResponse('<br>'.join(content))
     '''
 
+class PostDetailView(LoginRequiredMixin, DetailView):
+    """Return post detail"""
+
+    template_name = 'posts/detail.html'
+    queryset = Post.objects.all()
+    context_object_name = 'post'
+
+
+class CreatePostView(LoginRequiredMixin, CreateView):
+    """Create a new post."""
+
+    template_name = 'posts/new.html'
+    form_class = PostForm
+    success_url = reverse_lazy('posts:feed')
+
+    def get_context_data(self, **kwargs):
+        """Add user and profile to context."""
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        context['profile'] = self.request.user.profile
+        return context
+
+
+
 
 @login_required
 def create_post(request):
@@ -36,7 +75,7 @@ def create_post(request):
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('feed')
+            return redirect('posts:feed')
     else:
         form = PostForm()
 
